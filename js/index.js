1ce409658e29282821b2ba71cb1311218addabf9
noobnur <nurhsans@gmail.com>
2019-01-27 19:01:55 +0800

    PV001: Compute salary breakdown with 'computed'

--- js/index.js
+++ js/index.js
@@ -2,23 +2,41 @@
 var budgetform = {
   props: {
     type: String,
-    placeholder: String
+    placeholder: String,
+    salary: Number
   },
   template: `
   <div>
     <form class="budget-form">
-      <input type="number" min=0 max="100" v-bind:placeholder="placeholder"/>
-      <label><h5> % : {{type}} {{min}} </h5></label>
+      <input type="number" min=0 max="100" v-bind:placeholder="placeholder" v-model.number="value"/>
+      <label><h5> % : {{type}}</h5></label>
     </form>
-  </div>`
+    <h5>{{amountcomputed}} </h5>
+  </div>`,
+  data() {
+    return {
+      value: 0,
+      amount: ''
+    }
+  },
+  methods: {},
+  computed: {
+    amountcomputed() {
+      return "Amount: " + ((this.value/100) * this.salary)
+    }
+  }
 }
 
 new Vue({
   el: '#cw',
   components: {
     budgetform
+  },
+  data: {
+    salary: 3000
   }
 })
+
 
 
 
